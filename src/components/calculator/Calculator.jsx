b11a7bf3877f07a89e9b94f52047ee894bf82ddc
Aslanbek <kaipaeff@gmail.com>
2023-08-23 23:14:10 +0300

    added buttons

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import style from './calculator.module.css'
 
+import Buttons from '../../buttons/Buttons'
+
 export default function Calculator() {
   return (
     <div className={style.container}>
       <div className={style.output}>
-        Calculator
+        <input type="text" defaultValue='0' />
+      </div>
+      <div className={style.buttons}>
+        {Buttons.buttons.map(el => <button>{el.val}</button>)}
       </div>
     </div>
   );
