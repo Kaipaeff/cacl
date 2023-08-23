98fc973227c8c1d83391598a41e526ecfed137e6
Aslanbek <kaipaeff@gmail.com>
2023-08-23 22:40:41 +0300

    App style added

---
+++ src/calculator/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './calculator.module.css'
+
+export default function Calculator() {
+  return (
+    <div className={style.container}>
+      <div className={style.output}>
+        Calculator
+      </div>
+    </div>
+  );
+}
