94e0d794a23fe341f972db2df233c0afab3dbfc9
Aslanbek <kaipaeff@gmail.com>
2023-08-24 02:58:45 +0300

    styles added

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
-import style from './calculator.module.css'
+import './calculator.css'
 
 import Buttons from '../../buttons/Buttons'
 
 export default function Calculator() {
 
+
   const [value, setValue] = useState('0')
 
+
   return (
-    <div className={style.container}>
-      <div className={style.output}>
+    <div className='container'>
+
+      <div className='output'>
         <input type="text" defaultValue={value} />
       </div>
-      <div className={style.buttons}>
-        {Buttons.buttons.map(el => <button>{el.val}</button>)}
+
+      <div className='buttons'>
+        {Buttons.map(el => <button className={el.operation ? 'button operation' : 'button'} key={el.id}>{el.val}</button>)}
       </div>
+
     </div>
   );
 }
