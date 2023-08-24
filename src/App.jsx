67368aa9668faf42b2bee19598777ac5ff9b9e37
Aslanbek Kaipaev <kaipaeff@gmail.com>
2023-08-24 23:10:20 +0300

    Dev (#2)
    * App style added
    
    * added buttons
    
    * buttons mapped
    
    * styles added
    
    * some styles added
    
    * styles corrected
    
    * styles finished
    
    * add handlers
    
    * operators buttons processed
    
    * package.json modified
    
    * folders renamed
    
    * resolved conflicts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Calculator from './components/calculator/Calculator';
+import './app.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
