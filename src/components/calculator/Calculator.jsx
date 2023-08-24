5c9277d81a4059a86e17c5ec2a66a8ae5ae85a8b
Aslanbek <kaipaeff@gmail.com>
2023-08-24 20:05:11 +0300

    add handlers

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,23 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import './calculator.css'
 
 import Buttons from '../../buttons/Buttons'
 
 export default function Calculator() {
 
+  const [value, setValue] = useState('');
+  const inputRef = useRef(null);
 
-  const [value, setValue] = useState('0')
+  useEffect(() => {
+    inputRef.current.focus();
+  })
+
+
+  const handleClick = (val) => {
+    if (val !== 'AC') {
+      setValue((prev) => prev + val);
+    } else {
+      setValue('');
+    }
+  }
+
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value)
+  }
 
 
   return (
     <div className='container'>
-
       <div className='output'>
-        <input type="text" defaultValue={value} />
+        <input
+          ref={inputRef}
+          type="text"
+          value={value}
+          onChange={handleInputChange}
+          placeholder="0"
+        />
       </div>
 
       <div className='buttons'>
-        {Buttons.map(el => <button className={el.operation ? 'button operation' : 'button'} key={el.id}>{el.val}</button>)}
+        {Buttons.map(el =>
+          <button
+            onClick={() => { handleClick(el.val) }}
+            className={el.operation ? 'button operation' : 'button'}
+            key={el.id}>
+            {el.val}
+          </button>
+        )}
       </div>
 
     </div>
